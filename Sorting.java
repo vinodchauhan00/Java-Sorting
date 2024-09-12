57538ddbeb325ac823c545bdd21a11926c09e555
Vinod Chauhan <136825472+vinodchauhan00@users.noreply.github.com>
2024-09-12 19:44:34 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,26 @@
+package Practice;
+
+public class Sorting {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 9};
+        
+        int temp=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+
+            }
+            }
+        }
+        //java Enhanced for loop
+        
+        for(int k:arr){
+            System.out.print(k+" ");
+        }
+        // System.out.println("second largest elements:"+ arr[arr.length-2]);
+    }
+    
+}
